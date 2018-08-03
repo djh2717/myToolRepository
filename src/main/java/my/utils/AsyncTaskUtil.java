3d30ef2639a72a8718c1438350e19258e2c4593e
djh <1544579459@qq.com>
2018-08-03 10:50:53 +0800

    add a okHttpUtil and modify some other utils

---
+++ src/main/java/my/utils/AsyncTaskUtil.java
@@ -0,0 +1,8 @@
+package my.utils;
+
+/**
+ * @author djh on  2018/8/3 10:26
+ * @E-Mail 1544579459@qq.com
+ */
+public class AsyncTaskUtil {
+}
---
+++ src/main/java/my/utils/OkHttpUtil.java
@@ -0,0 +1,8 @@
+package my.utils;
+
+/**
+ * @author djh on  2018/8/2 16:31
+ * @E-Mail 1544579459@qq.com
+ */
+public class OkHttpUtil {
+}
