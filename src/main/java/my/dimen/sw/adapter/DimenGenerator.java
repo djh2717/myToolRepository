09f9ad393ed4a3782f0c6daa304355374944e5d7
djh <1544579459@qq.com>
2018-08-15 18:39:19 +0800

    add a dimen adapter, use sw to implement

---
+++ src/main/java/my/dimen/sw/adapter/DimenGenerator.java
@@ -0,0 +1,28 @@
+package my.dimen.sw.adapter;
+
+
+/**
+ * @author 15445
+ */
+public class DimenGenerator {
+
+    /**
+     * 设计稿尺寸(根据自己设计师的设计稿的宽度填入)
+     */
+    private static final int DESIGN_WIDTH = 1440;
+
+    /**
+     * 设计稿高度  没用到
+     */
+    private static final int DESIGN_HEIGHT = 667;
+
+    public static void main(String[] args) {
+
+        DimenTypes[] values = DimenTypes.values();
+        for (DimenTypes value : values) {
+            MakeUtils.makeAll(DESIGN_WIDTH, value, "/dimenAdapter");
+        }
+
+    }
+
+}
